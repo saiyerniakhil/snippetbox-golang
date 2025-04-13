9b101a5a04269b76d66125e42f046206ebc466c1
saiyerniakhil <makhil123@yahoo.in>
2025-04-12 17:09:46 -0700

    add templates, static files

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,16 +10,25 @@ import (
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	ts, err := template.ParseFiles("./ui/html/home.tmpl.html")
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/partials/nav.tmpl.html",
+		"./ui/html/partials/header.tmpl.html",
+		"./ui/html/partials/footer.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
-	err = ts.Execute(w, nil)
-	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-	}
+
 	fmt.Fprintf(w, "Hello from Snippetbox!")
 }
 
