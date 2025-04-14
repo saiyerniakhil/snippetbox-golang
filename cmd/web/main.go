0853a74dedded872f11927e5cbcdf950848d6015
saiyerniakhil <makhil123@yahoo.in>
2025-04-13 18:14:18 -0700

    feat: add mysql driver and connection details

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -14,6 +17,8 @@ type application struct {
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP Network Address")
+	dsn := flag.String("dsn", "web:my-secret-pw@tcp(localhost:3306)/snippetbox?parseTime=true", "MYSQL Data Source Name")
+
 	flag.Parse()
 
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
@@ -21,10 +26,31 @@ func main() {
 	app := &application{
 		logger: logger,
 	}
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+	}
+
+	defer db.Close()
 
 	logger.Info("starting server on", "addr", *addr)
 
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
